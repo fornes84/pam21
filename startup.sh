b0839a75eeabe5f87fd1a82ff67f885fa0c1033a
isx43457008 <isx43457008@i22.informatica.escoladeltreball.org>
2021-11-02 12:00:27 +0100

    psuh

---
+++ startup.sh
@@ -0,0 +1,12 @@
+#! /bin/bash
+
+useradd -m -s /bin/bash unix01
+useradd -m -s /bin/bash unix02
+useradd -m -s /bin/bash unix03
+echo -e "unix01\nunix01" | passwd unix01
+echo -e "unix02\nunix02" | passwd unix02
+echo -e "unix03\nunix03" | passwd unix03
+
+cp /opt/docker/login.defs /etc/login.defs 
+
+/bin/bash
